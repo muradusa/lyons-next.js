d872de43d8894df0c948d67cda4ffdcdd3df279a
Murad Cholukov <muradusa@gmail.com>
2021-05-08 14:48:52 -0700

    adding landing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Navbar />
       <Component {...pageProps} />
       <Footer />
     </div>
