6dba6872fec3d0c8338904a1afeaaddb939758d6
Murad Cholukov <muradusa@gmail.com>
2021-06-15 22:35:38 -0700

    dynamic routing

--- components/Projects.js
+++ components/Projects.js
@@ -1,98 +1,79 @@
-/*
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/aspect-ratio'),
-    ]
-  }
-  ```
-*/
+import Link from "next/link";
+
 const files = [
   {
-    title: "IMG_4985.HEIC",
+    title: "Cupertino City Center",
     size: "3.9 MB",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "IMG_4985.HEIC",
+    title: "Park Avenue",
     size: "3.9 MB",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "IMG_4985.HEIC",
+    title: "UC Davis TP3",
     size: "3.9 MB",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "IMG_4985.HEIC",
+    title: "LIvermore Lab",
     size: "3.9 MB",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "IMG_4985.HEIC",
+    title: "Firestation",
     size: "3.9 MB",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "IMG_4985.HEIC",
+    title: "Concord_Airport",
     size: "3.9 MB",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "IMG_4985.HEIC",
+    title: "Police Station",
     size: "3.9 MB",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "IMG_4985.HEIC",
+    title: "WIX Project",
     size: "3.9 MB",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "IMG_4985.HEIC",
+    title: "Ygnacio VIllage",
     size: "3.9 MB",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "IMG_4985.HEIC",
+    title: "FM Industries",
     size: "3.9 MB",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "IMG_4985.HEIC",
+    title: "Next Project",
     size: "3.9 MB",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "IMG_4985.HEIC",
+    title: "Ashgabat CIty",
     size: "3.9 MB",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
-  {
-    title: "IMG_4985.HEIC",
-    size: "3.9 MB",
-    source:
-      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-  },
+
   // More files...
 ];
 
@@ -104,19 +85,24 @@ export default function Projects() {
     >
       {files.map((file) => (
         <li key={file.source} className="relative">
-          <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
-            <img
-              src={file.source}
-              alt=""
-              className="object-cover pointer-events-none group-hover:opacity-75"
-            />
-            <button
-              type="button"
-              className="absolute inset-0 focus:outline-none"
-            >
-              <span className="sr-only">View details for {file.title}</span>
-            </button>
-          </div>
+          <Link href={`/projects/${file.title}`}>
+            <a>
+              <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
+                <img
+                  src={file.source}
+                  alt=""
+                  className="object-cover pointer-events-none group-hover:opacity-75"
+                />
+                <button
+                  type="button"
+                  className="absolute inset-0 focus:outline-none"
+                >
+                  <span className="sr-only">View details for {file.title}</span>
+                </button>
+              </div>
+            </a>
+          </Link>
+
           <p className="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">
             {file.title}
           </p>
