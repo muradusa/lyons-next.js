af449d5ae48d97173f447997303e2d7b3ca41df4
Murad Cholukov <muradusa@gmail.com>
2021-07-01 15:43:57 -0700

    adding FM industries photos

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,38 +1,38 @@
-import React from "react";
+// import React from "react";
 
-function Project() {
-  return (
-    <div>
-      <p>Hello this is slug.js</p>
-    </div>
-  );
-}
+// function Project() {
+//   return (
+//     <div>
+//       <p>Hello this is slug.js</p>
+//     </div>
+//   );
+// }
 
-// This function gets called at build time
-export async function getStaticPaths() {
-  // Call an external API endpoint to get posts
-  const res = await fetch(`http://localhost:3000/api`);
-  const projects = await res.json();
+// // This function gets called at build time
+// export async function getStaticPaths() {
+//   // Call an external API endpoint to get posts
+//   const res = await fetch(`http://localhost:3000/api`);
+//   const projects = await res.json();
 
-  // Get the paths we want to pre-render based on posts
-  const paths = projects.map((project) => ({
-    params: { id: project.asset_id.toString() },
-  }));
+//   // Get the paths we want to pre-render based on posts
+//   const paths = projects.map((project) => ({
+//     params: { id: project.asset_id.toString() },
+//   }));
 
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
-  return { paths, fallback: false };
-}
+//   // We'll pre-render only these paths at build time.
+//   // { fallback: false } means other routes should 404.
+//   return { paths, fallback: false };
+// }
 
-// This also gets called at build time
-export async function getStaticProps({ params }) {
-  // params contains the post `id`.
-  // If the route is like /posts/1, then params.id is 1
-  const res = await fetch(`http://localhost:3000/api/${params.id}`);
-  const project = await res.json();
+// // This also gets called at build time
+// export async function getStaticProps({ params }) {
+//   // params contains the post `id`.
+//   // If the route is like /posts/1, then params.id is 1
+//   const res = await fetch(`http://localhost:3000/api/${params.id}`);
+//   const project = await res.json();
 
-  // Pass post data to the page via props
-  return { props: { project } };
-}
+//   // Pass post data to the page via props
+//   return { props: { project } };
+// }
 
-export default Project;
+// export default Project;
---
+++ pages/projects/fmIndustries.js
@@ -0,0 +1,22 @@
+import ImageGallery from "react-image-gallery";
+
+const images = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+];
+
+function fm_industries() {
+  return <ImageGallery items={images} fullscreen="true" />;
+}
+
+export default fm_industries;
