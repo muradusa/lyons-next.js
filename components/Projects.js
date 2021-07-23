1fc8af6e56243ad429639a34b0177e4762be109e
muradusa <muradusa@gmail.com>
2021-07-22 19:55:56 -0700

    projetc page

--- components/Projects.js
+++ components/Projects.js
@@ -3,73 +3,37 @@ import Link from "next/link";
 const files = [
   {
     title: "Cupertino City Center",
-    size: "3.9 MB",
+    slug: "cupertino",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
     title: "Park Avenue",
-    size: "3.9 MB",
+    slug: "park",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
     title: "UC Davis TP3",
-    size: "3.9 MB",
+    slug: "ucd",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
     title: "Livermore Lab",
-    size: "3.9 MB",
+    slug: "livermore",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
     title: "Firestation",
-    size: "3.9 MB",
+    slug: "firestation",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
   {
-    title: "Concord_Airport",
-    size: "3.9 MB",
-    source:
-      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-  },
-  {
-    title: "Police Station",
-    size: "3.9 MB",
-    source:
-      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-  },
-  {
-    title: "WIX Project",
-    size: "3.9 MB",
-    source:
-      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-  },
-  {
-    title: "FM Indusstries",
-    size: "3.9 MB",
-    source:
-      "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2183/v1623889912/Lyons/FM%20Industries/FM.jpg",
-  },
-  {
-    title: "Ygnacio Valley",
-    size: "3.9 MB",
-    source:
-      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-  },
-  {
-    title: "Next Project",
-    size: "3.9 MB",
-    source:
-      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-  },
-  {
-    title: "Ashgabat CIty",
-    size: "3.9 MB",
+    title: "Airport",
+    slug: "airport",
     source:
       "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
   },
@@ -85,7 +49,7 @@ export default function Projects(props) {
     >
       {files.map((file) => (
         <li key={file.source} className="relative">
-          <Link href={`/projects/${file.title}`}>
+          <Link href={`/projects/${file.slug}`}>
             <a>
               <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
                 <img
@@ -107,9 +71,9 @@ export default function Projects(props) {
           <p className="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">
             {file.title}
           </p>
-          <p className="block text-sm font-medium text-gray-500 pointer-events-none">
+          {/* <p className="block text-sm font-medium text-gray-500 pointer-events-none">
             {file.size}
-          </p>
+          </p> */}
         </li>
       ))}
     </ul>
--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,13 +1,85 @@
 import React from "react";
 import { useRouter } from "next/dist/client/router";
 
+import ImageGallery from "react-image-gallery";
+
+const projects = {
+  cupertino: [
+    {
+      original:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1623820286/samples/landscapes/nature-mountains.webp",
+      thumbnail:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1623820286/samples/landscapes/nature-mountains.webp",
+    },
+    {
+      original:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1626796077/Lyons/FM%20Industries/Lyons_FM_Industries_FM-c_scale_w_2183_yz5cwt.webp",
+      thumbnail:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1626796077/Lyons/FM%20Industries/Lyons_FM_Industries_FM-c_scale_w_2183_yz5cwt.webp",
+    },
+    {
+      original:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1626827029/Lyons/FM%20Industries/2021-06-09_LyonsConstruction_FMIndustriesDJI_0604_LRG_vfofrn.webp",
+
+      thumbnail:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1626827029/Lyons/FM%20Industries/2021-06-09_LyonsConstruction_FMIndustriesDJI_0604_LRG_vfofrn.webp",
+    },
+  ],
+  livermore: [
+    {
+      original:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1623820286/samples/landscapes/nature-mountains.webp",
+      thumbnail:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1623820286/samples/landscapes/nature-mountains.webp",
+    },
+    {
+      original:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1626796077/Lyons/FM%20Industries/Lyons_FM_Industries_FM-c_scale_w_2183_yz5cwt.webp",
+      thumbnail:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1626796077/Lyons/FM%20Industries/Lyons_FM_Industries_FM-c_scale_w_2183_yz5cwt.webp",
+    },
+    {
+      original:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1626827029/Lyons/FM%20Industries/2021-06-09_LyonsConstruction_FMIndustriesDJI_0604_LRG_vfofrn.webp",
+
+      thumbnail:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1626827029/Lyons/FM%20Industries/2021-06-09_LyonsConstruction_FMIndustriesDJI_0604_LRG_vfofrn.webp",
+    },
+  ],
+};
+
+// const Cupertino = [
+//   {
+//     original:
+//       "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1623820286/samples/landscapes/nature-mountains.webp",
+//     thumbnail:
+//       "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1623820286/samples/landscapes/nature-mountains.webp",
+//   },
+//   {
+//     original:
+//       "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1626796077/Lyons/FM%20Industries/Lyons_FM_Industries_FM-c_scale_w_2183_yz5cwt.webp",
+//     thumbnail:
+//       "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1626796077/Lyons/FM%20Industries/Lyons_FM_Industries_FM-c_scale_w_2183_yz5cwt.webp",
+//   },
+//   {
+//     original:
+//       "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1626827029/Lyons/FM%20Industries/2021-06-09_LyonsConstruction_FMIndustriesDJI_0604_LRG_vfofrn.webp",
+
+//     thumbnail:
+//       "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1626827029/Lyons/FM%20Industries/2021-06-09_LyonsConstruction_FMIndustriesDJI_0604_LRG_vfofrn.webp",
+//   },
+// ];
+
 function Project() {
   const router = useRouter();
   const { slug } = router.query;
+  const name = slug;
 
   return (
     <div>
       <p>Hello this is {slug}</p>
+      <p>{name}</p>
+      {/* <ImageGallery items={projects.slug} fullscreen="true" /> */}
     </div>
   );
 }
