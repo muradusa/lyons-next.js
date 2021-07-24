67248542c4549c888d39188abba67a1a2c1b331e
muradusa <muradusa@gmail.com>
2021-07-24 10:24:22 -0700

    dynamic project page

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useRouter } from "next/dist/client/router";
+const _ = require("lodash");
 
 import ImageGallery from "react-image-gallery";
 
@@ -28,58 +29,39 @@ const projects = {
   livermore: [
     {
       original:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1623820286/samples/landscapes/nature-mountains.webp",
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1623820284/samples/ecommerce/accessories-bag.jpg",
       thumbnail:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1623820286/samples/landscapes/nature-mountains.webp",
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1623820284/samples/ecommerce/accessories-bag.jpg",
     },
     {
       original:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1626796077/Lyons/FM%20Industries/Lyons_FM_Industries_FM-c_scale_w_2183_yz5cwt.webp",
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1623820284/samples/ecommerce/leather-bag-gray.jpg",
       thumbnail:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1626796077/Lyons/FM%20Industries/Lyons_FM_Industries_FM-c_scale_w_2183_yz5cwt.webp",
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1623820284/samples/ecommerce/leather-bag-gray.jpg",
     },
     {
       original:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1626827029/Lyons/FM%20Industries/2021-06-09_LyonsConstruction_FMIndustriesDJI_0604_LRG_vfofrn.webp",
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1623820276/samples/ecommerce/analog-classic.jpg",
 
       thumbnail:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1626827029/Lyons/FM%20Industries/2021-06-09_LyonsConstruction_FMIndustriesDJI_0604_LRG_vfofrn.webp",
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1623820276/samples/ecommerce/analog-classic.jpg",
     },
   ],
 };
 
-// const Cupertino = [
-//   {
-//     original:
-//       "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1623820286/samples/landscapes/nature-mountains.webp",
-//     thumbnail:
-//       "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1623820286/samples/landscapes/nature-mountains.webp",
-//   },
-//   {
-//     original:
-//       "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1626796077/Lyons/FM%20Industries/Lyons_FM_Industries_FM-c_scale_w_2183_yz5cwt.webp",
-//     thumbnail:
-//       "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1626796077/Lyons/FM%20Industries/Lyons_FM_Industries_FM-c_scale_w_2183_yz5cwt.webp",
-//   },
-//   {
-//     original:
-//       "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1626827029/Lyons/FM%20Industries/2021-06-09_LyonsConstruction_FMIndustriesDJI_0604_LRG_vfofrn.webp",
-
-//     thumbnail:
-//       "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1626827029/Lyons/FM%20Industries/2021-06-09_LyonsConstruction_FMIndustriesDJI_0604_LRG_vfofrn.webp",
-//   },
-// ];
-
 function Project() {
   const router = useRouter();
   const { slug } = router.query;
-  const name = slug;
+
+  const allowed = [slug];
+  const filteredObj = _.pick(projects, allowed);
+
+  const photos = filteredObj[slug];
 
   return (
     <div>
       <p>Hello this is {slug}</p>
-      <p>{name}</p>
-      {/* <ImageGallery items={projects.slug} fullscreen="true" /> */}
+      {photos && <ImageGallery items={photos} fullscreen="true" />}
     </div>
   );
 }
