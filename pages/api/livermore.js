36914b7e872f52788e02e01bf0cea3df847bf9e1
muradusa <muradusa@gmail.com>
2021-07-25 14:45:49 -0700

    dynamic project page

---
+++ pages/api/livermore.js
@@ -0,0 +1,20 @@
+import cloudinary from "cloudinary";
+
+export default function handler(req, res) {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY,
+    secure: true,
+  });
+
+  cloudinary.v2.api.resources(
+    {
+      type: "upload",
+      prefix: "Lyons/livermore", // add your folder
+    },
+    function (error, result) {
+      res.status(200).json(result.resources);
+    }
+  );
+}
--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/dist/client/router";
-const _ = require("lodash");
+import _ from "lodash";
+import axios from "axios";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
-import ImageGallery from "react-image-gallery";
+// import ImageGallery from "react-image-gallery";
+import Image from "next/image";
 
 const projects = {
   cupertino: [
@@ -17,13 +20,6 @@ const projects = {
         "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1626796077/Lyons/FM%20Industries/Lyons_FM_Industries_FM-c_scale_w_2183_yz5cwt.webp",
       thumbnail:
         "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1626796077/Lyons/FM%20Industries/Lyons_FM_Industries_FM-c_scale_w_2183_yz5cwt.webp",
-    },
-    {
-      original:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1626827029/Lyons/FM%20Industries/2021-06-09_LyonsConstruction_FMIndustriesDJI_0604_LRG_vfofrn.webp",
-
-      thumbnail:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1626827029/Lyons/FM%20Industries/2021-06-09_LyonsConstruction_FMIndustriesDJI_0604_LRG_vfofrn.webp",
     },
   ],
   livermore: [
@@ -54,14 +50,48 @@ function Project() {
   const { slug } = router.query;
 
   const allowed = [slug];
-  const filteredObj = _.pick(projects, allowed);
+  // const filteredObj = _.pick(projects, allowed);
+  // const photos = filteredObj[slug];
 
-  const photos = filteredObj[slug];
+  const [pics, setPics] = useState([]);
+  const handle = useFullScreenHandle();
+
+  useEffect(() => {
+    axios
+
+      .get(`http://localhost:3000/api/${allowed}`)
+      .then((res) => {
+        setPics(res.data);
+      })
+
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <div>
-      <p>Hello this is {slug}</p>
-      {photos && <ImageGallery items={photos} fullscreen="true" />}
+      <p className="ml-5">Hello this is {slug}</p>
+
+      <ul
+        role="list"
+        className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8 p-5"
+      >
+        {pics.map((pic) => (
+          <li key={pic.asset_id} className="relative">
+            <FullScreen handle={handle}>
+              <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
+                <Image
+                  src={pic.url}
+                  layout="fill"
+                  onClick={handle.enter}
+                  className="object-cover  group-hover:opacity-75 cursor-pointer"
+                />
+              </div>
+            </FullScreen>
+          </li>
+        ))}
+      </ul>
+
+      {/* {photos && <ImageGallery items={photos} fullscreen="true" />} */}
     </div>
   );
 }
