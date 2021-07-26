36161ba4ea96b8465b02070e527ca4da61cf6a00
Murad Cholukov <muradusa@gmail.com>
2021-07-26 09:40:11 -0700

    adding images from cloudinary

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -2,24 +2,83 @@ import React, { useState, useEffect } from "react";
 import { useRouter } from "next/dist/client/router";
 import _ from "lodash";
 import axios from "axios";
-import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
-// import ImageGallery from "react-image-gallery";
+import ImageGallery from "react-image-gallery";
 import Image from "next/image";
 
 const projects = {
   cupertino: [
     {
       original:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1623820286/samples/landscapes/nature-mountains.webp",
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316663/Lyons/cupertino/11_g4rrut.jpg",
       thumbnail:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1623820286/samples/landscapes/nature-mountains.webp",
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316663/Lyons/cupertino/11_g4rrut.jpg",
     },
     {
       original:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1626796077/Lyons/FM%20Industries/Lyons_FM_Industries_FM-c_scale_w_2183_yz5cwt.webp",
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316663/Lyons/cupertino/7_ob6pvn.jpg",
       thumbnail:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1626796077/Lyons/FM%20Industries/Lyons_FM_Industries_FM-c_scale_w_2183_yz5cwt.webp",
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316663/Lyons/cupertino/7_ob6pvn.jpg",
+    },
+    {
+      original:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316663/Lyons/cupertino/5_hbjpfe.jpg",
+      thumbnail:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316663/Lyons/cupertino/5_hbjpfe.jpg",
+    },
+    {
+      original:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/12_jamivp.jpg",
+      thumbnail:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/12_jamivp.jpg",
+    },
+    {
+      original:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/9_lvkere.jpg",
+      thumbnail:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/9_lvkere.jpg",
+    },
+    {
+      original:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/8_wevg0v.jpg",
+      thumbnail:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/8_wevg0v.jpg",
+    },
+    {
+      original:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/4_e03nmr.jpg",
+      thumbnail:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/4_e03nmr.jpg",
+    },
+    {
+      original:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/10_kayjab.jpg",
+      thumbnail:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/10_kayjab.jpg",
+    },
+    {
+      original:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316661/Lyons/cupertino/6_mqwfet.jpg",
+      thumbnail:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316661/Lyons/cupertino/6_mqwfet.jpg",
+    },
+    {
+      original:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316661/Lyons/cupertino/2_io0wmq.jpg",
+      thumbnail:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316661/Lyons/cupertino/2_io0wmq.jpg",
+    },
+    {
+      original:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316661/Lyons/cupertino/3_cnh4cx.jpg",
+      thumbnail:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316661/Lyons/cupertino/3_cnh4cx.jpg",
+    },
+    {
+      original:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316661/Lyons/cupertino/1_qvht9z.jpg",
+      thumbnail:
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316661/Lyons/cupertino/1_qvht9z.jpg",
     },
   ],
   livermore: [
@@ -50,48 +109,45 @@ function Project() {
   const { slug } = router.query;
 
   const allowed = [slug];
-  // const filteredObj = _.pick(projects, allowed);
-  // const photos = filteredObj[slug];
+  const filteredObj = _.pick(projects, allowed);
+  const photos = filteredObj[slug];
 
-  const [pics, setPics] = useState([]);
-  const handle = useFullScreenHandle();
+  // const [pics, setPics] = useState([]);
 
-  useEffect(() => {
-    axios
+  // useEffect(() => {
+  //   axios
 
-      .get(`http://localhost:3000/api/${allowed}`)
-      .then((res) => {
-        setPics(res.data);
-      })
+  //     .get(`http://localhost:3000/api/${allowed}`)
+  //     .then((res) => {
+  //       setPics(res.data);
+  //     })
 
-      .catch((error) => console.log(error));
-  }, []);
+  //     .catch((error) => console.log(error));
+  // }, []);
 
   return (
     <div>
       <p className="ml-5">Hello this is {slug}</p>
 
-      <ul
+      {/* <ul
         role="list"
         className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8 p-5"
       >
         {pics.map((pic) => (
           <li key={pic.asset_id} className="relative">
-            <FullScreen handle={handle}>
-              <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
-                <Image
-                  src={pic.url}
-                  layout="fill"
-                  onClick={handle.enter}
-                  className="object-cover  group-hover:opacity-75 cursor-pointer"
-                />
-              </div>
-            </FullScreen>
+            <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
+              <Image
+                src={pic.url}
+                layout="fill"
+                onClick={handle.enter}
+                className="object-cover  group-hover:opacity-75 cursor-pointer"
+              />
+            </div>
           </li>
         ))}
-      </ul>
+      </ul> */}
 
-      {/* {photos && <ImageGallery items={photos} fullscreen="true" />} */}
+      {photos && <ImageGallery items={photos} fullscreen="true" />}
     </div>
   );
 }
