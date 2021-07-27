069af5e3cbe7c6bc1b2484f7d42e33f7d97fbbfa
muradusa <muradusa@gmail.com>
2021-07-27 07:29:05 -0700

    fetching data from local server

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,1 +1,47 @@
-export default function handler(req, res) {}
+const projects = [
+  {
+    name: "cupertino",
+    photos: [
+      {
+        original:
+          "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1623820286/samples/landscapes/nature-mountains.webp",
+        thumbnail:
+          "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1623820286/samples/landscapes/nature-mountains.webp",
+      },
+      {
+        original:
+          "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1626796077/Lyons/FM%20Industries/Lyons_FM_Industries_FM-c_scale_w_2183_yz5cwt.webp",
+        thumbnail:
+          "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1626796077/Lyons/FM%20Industries/Lyons_FM_Industries_FM-c_scale_w_2183_yz5cwt.webp",
+      },
+    ],
+  },
+  {
+    name: "livermore",
+    photos: [
+      {
+        original:
+          "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1623820284/samples/ecommerce/accessories-bag.jpg",
+        thumbnail:
+          "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1623820284/samples/ecommerce/accessories-bag.jpg",
+      },
+      {
+        original:
+          "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1623820284/samples/ecommerce/leather-bag-gray.jpg",
+        thumbnail:
+          "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1623820284/samples/ecommerce/leather-bag-gray.jpg",
+      },
+      {
+        original:
+          "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1623820276/samples/ecommerce/analog-classic.jpg",
+
+        thumbnail:
+          "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1623820276/samples/ecommerce/analog-classic.jpg",
+      },
+    ],
+  },
+];
+
+export default function handler(req, res) {
+  res.status(200).json(projects);
+}
--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -10,75 +10,15 @@ const projects = {
   cupertino: [
     {
       original:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316663/Lyons/cupertino/11_g4rrut.jpg",
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1623820286/samples/landscapes/nature-mountains.webp",
       thumbnail:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316663/Lyons/cupertino/11_g4rrut.jpg",
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1623820286/samples/landscapes/nature-mountains.webp",
     },
     {
       original:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316663/Lyons/cupertino/7_ob6pvn.jpg",
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_scale,w_2500/v1626796077/Lyons/FM%20Industries/Lyons_FM_Industries_FM-c_scale_w_2183_yz5cwt.webp",
       thumbnail:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316663/Lyons/cupertino/7_ob6pvn.jpg",
-    },
-    {
-      original:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316663/Lyons/cupertino/5_hbjpfe.jpg",
-      thumbnail:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316663/Lyons/cupertino/5_hbjpfe.jpg",
-    },
-    {
-      original:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/12_jamivp.jpg",
-      thumbnail:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/12_jamivp.jpg",
-    },
-    {
-      original:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/9_lvkere.jpg",
-      thumbnail:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/9_lvkere.jpg",
-    },
-    {
-      original:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/8_wevg0v.jpg",
-      thumbnail:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/8_wevg0v.jpg",
-    },
-    {
-      original:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/4_e03nmr.jpg",
-      thumbnail:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/4_e03nmr.jpg",
-    },
-    {
-      original:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/10_kayjab.jpg",
-      thumbnail:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316662/Lyons/cupertino/10_kayjab.jpg",
-    },
-    {
-      original:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316661/Lyons/cupertino/6_mqwfet.jpg",
-      thumbnail:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316661/Lyons/cupertino/6_mqwfet.jpg",
-    },
-    {
-      original:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316661/Lyons/cupertino/2_io0wmq.jpg",
-      thumbnail:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316661/Lyons/cupertino/2_io0wmq.jpg",
-    },
-    {
-      original:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316661/Lyons/cupertino/3_cnh4cx.jpg",
-      thumbnail:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316661/Lyons/cupertino/3_cnh4cx.jpg",
-    },
-    {
-      original:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316661/Lyons/cupertino/1_qvht9z.jpg",
-      thumbnail:
-        "https://res.cloudinary.com/dz7rkbzdw/image/upload/v1627316661/Lyons/cupertino/1_qvht9z.jpg",
+        "https://res.cloudinary.com/dz7rkbzdw/image/upload/c_thumb,w_200/v1626796077/Lyons/FM%20Industries/Lyons_FM_Industries_FM-c_scale_w_2183_yz5cwt.webp",
     },
   ],
   livermore: [
@@ -104,13 +44,15 @@ const projects = {
   ],
 };
 
-function Project() {
+function Project({ project }) {
   const router = useRouter();
   const { slug } = router.query;
 
-  const allowed = [slug];
-  const filteredObj = _.pick(projects, allowed);
-  const photos = filteredObj[slug];
+  // const allowed = [slug];
+  // const filteredObj = _.pick(projects, allowed);
+  // const photos = filteredObj[slug];
+
+  console.log(project);
 
   // const [pics, setPics] = useState([]);
 
@@ -129,54 +71,54 @@ function Project() {
     <div>
       <p className="ml-5">Hello this is {slug}</p>
 
-      {/* <ul
+      <ul
         role="list"
         className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8 p-5"
       >
-        {pics.map((pic) => (
-          <li key={pic.asset_id} className="relative">
+        {project.map((photo) => (
+          <li key={photo.asset_id} className="relative">
             <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
               <Image
-                src={pic.url}
+                src={photo.url}
                 layout="fill"
-                onClick={handle.enter}
+                // onClick={handle.enter}
                 className="object-cover  group-hover:opacity-75 cursor-pointer"
               />
             </div>
           </li>
         ))}
-      </ul> */}
-
-      {photos && <ImageGallery items={photos} fullscreen="true" />}
+      </ul>
+      {/* 
+      {photos && <ImageGallery items={photos} fullscreen="true" />} */}
     </div>
   );
 }
 
-// // This function gets called at build time
-// export async function getStaticPaths() {
-//   // Call an external API endpoint to get posts
-//   const res = await fetch(`http://localhost:3000/api`);
-//   const projects = await res.json();
+// This function gets called at build time
+export async function getStaticPaths() {
+  // Call an external API endpoint to get posts
+  const res = await fetch(`http://localhost:3000/api`);
+  const projects = await res.json();
 
-//   // Get the paths we want to pre-render based on posts
-//   const paths = projects.map((project) => ({
-//     params: { id: project.asset_id.toString() },
-//   }));
+  // Get the paths we want to pre-render based on posts
+  const paths = projects.map((project) => ({
+    params: { slug: project.name.toString() },
+  }));
 
-//   // We'll pre-render only these paths at build time.
-//   // { fallback: false } means other routes should 404.
-//   return { paths, fallback: false };
-// }
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false };
+}
 
-// // This also gets called at build time
-// export async function getStaticProps({ params }) {
-//   // params contains the post `id`.
-//   // If the route is like /posts/1, then params.id is 1
-//   const res = await fetch(`http://localhost:3000/api/${params.id}`);
-//   const project = await res.json();
+// This also gets called at build time
+export async function getStaticProps({ params }) {
+  // params contains the post `id`.
+  // If the route is like /posts/1, then params.id is 1
+  const res = await fetch(`http://localhost:3000/api/${params.slug}`);
+  const project = await res.json();
 
-//   // Pass post data to the page via props
-//   return { props: { project } };
-// }
+  // Pass post data to the page via props
+  return { props: { project } };
+}
 
 export default Project;
