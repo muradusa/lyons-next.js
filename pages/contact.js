7853b8ffe8285d98b670e60e0d68327f832790c5
muradusa <muradusa@gmail.com>
2021-08-18 19:53:09 -0700

    updating curretn projects

--- pages/contact.js
+++ pages/contact.js
@@ -15,6 +15,7 @@ export default function Contact() {
         <Header
           title="Contact us"
           description="Please don't hesitate to contact us."
+          image="https://images.unsplash.com/photo-1596524430615-b46475ddff6e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80"
         />
         <ContactForm />
       </main>
